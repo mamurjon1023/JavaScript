c10cd08cda990f90a14b6f22690842937a62c1b2
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-05-24 18:51:44 +0500

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+let ism = prompt("Ismingizni kiriting");
+let familya = prompt("Familyangizni kiriting");
+let yosh = prompt("Yoshingizni kiriting");
+let fan = prompt("Yoqtirgan faningizni kiriting");
+let tuman = prompt("Yashaydigan tumaningizni kiriting");
+
+alert("Ma`lumotlar uchun rahmat")
